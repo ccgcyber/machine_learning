bd48683e3d40020522a180a78dfe594b8bd6d170
CCG CyberSecurity Group <ccg.cyber@gmail.com>
2017-07-19 07:57:57 -0400

    translate to english

--- sklearn/sklearn_demo.py
+++ sklearn/sklearn_demo.py
@@ -6,7 +6,7 @@ import time
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    ## step 1: 加载数据
+    ## step 1: Load data
     print "step 1: load data..."
 
     dataSet = []
@@ -15,21 +15,21 @@ if __name__ == '__main__':
         lineArr = line.strip().split(' ')
         dataSet.append([float(lineArr[0]), float(lineArr[1])])
 
-    #设定不同k值以运算
+    # Set different k values ​​to operate
     for k in range(2,10):
-        clf = KMeans(n_clusters=k) #设定k
-        s = clf.fit(dataSet) #加载数据集合
+        clf = KMeans(n_clusters=k) # Set k
+        s = clf.fit(dataSet) # Load the data collection
         numSamples = len(dataSet)
         centroids = clf.labels_
-        print centroids,type(centroids) #显示中心点
-        print clf.inertia_  #显示聚类效果
+        print centroids,type(centroids) # Show center point
+        print clf.inertia_  # Show clustering effects
         mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
-        #画出所有样例点 属于同一分类的绘制同样的颜色
+        # Draw all the sample points that belong to the same class to draw the same color
         for i in xrange(numSamples):
             #markIndex = int(clusterAssment[i, 0])
             plt.plot(dataSet[i][0], dataSet[i][1], mark[clf.labels_[i]]) #mark[markIndex])
         mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']
-        # 画出质点，用特殊图型
+        # Draw a particle, with a special pattern
         centroids =  clf.cluster_centers_
         for i in range(k):
             plt.plot(centroids[i][0], centroids[i][1], mark[i], markersize = 12)
