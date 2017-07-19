ec1d6678d8df8d0eccfa001251ae15d939ef1788
CCG CyberSecurity Group <ccg.cyber@gmail.com>
2017-07-19 07:02:49 -0400

    translate to english
    useing google translate

--- 2.verification_code/IMG_denoising/denoising.py
+++ 2.verification_code/IMG_denoising/denoising.py
@@ -8,16 +8,16 @@ img = img.convert('RGBA')
 img = img.convert('L')
 img.save('end_9.png')
 
-sharpness = ImageEnhance.Sharpness(img)#锐利化
+sharpness = ImageEnhance.Sharpness(img)# Sharpened
 img = sharpness.enhance(7.0)
 img.save('end_0.png')
-#img = ImageEnhance.Color(img) #变黑白
+#img = ImageEnhance.Color(img) # Black and white
 #img = img.enhance(0)
 img.save('end_1.png')
-#img = ImageEnhance.Brightness(img) # 提高亮度
+#img = ImageEnhance.Brightness(img) # Increase brightness
 #img = img.enhance(3)
 img.save('end_2.png')
-img = ImageEnhance.Contrast(img) # 高对比
+img = ImageEnhance.Contrast(img) # High contrast
 img = img.enhance(8)
 img.save('end_3.png')
 #img = ImageEnhance.Color(img)
