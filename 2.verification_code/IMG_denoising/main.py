b7cd705882005114b866fc200e490d6709a6bfb4
CCG CyberSecurity Group <ccg.cyber@gmail.com>
2017-07-19 07:04:15 -0400

    english translation

--- 2.verification_code/IMG_denoising/main.py
+++ 2.verification_code/IMG_denoising/main.py
@@ -4,7 +4,7 @@ from numpy import random
 from scipy.ndimage import filters
 import rof
 
-# 使用噪声创建合成图像
+# Use noise to create composite images
 im = zeros((500,500))
 im[100:400,100:400] = 128
 im[200:300,200:300] = 255
@@ -13,7 +13,7 @@ im = im + 30*random.standard_normal((500,500))
 U,T = rof.denoise(im,im)
 G = filters.gaussian_filter(im,10)
 
-# 保存生成结果
+# Save the generated result
 from scipy.misc import imsave
 imsave('synth_rof.pdf',U)
 imsave('synth_gaussian.pdf',G)
