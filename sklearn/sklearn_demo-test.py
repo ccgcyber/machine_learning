982388807ecb161838dc83c0b0cde178d9d9bd53
CCG CyberSecurity Group <ccg.cyber@gmail.com>
2017-07-19 07:55:30 -0400

    translate to english

--- sklearn/sklearn_demo-test.py
+++ sklearn/sklearn_demo-test.py
@@ -7,7 +7,7 @@ from sklearn.cluster import MeanShift, estimate_bandwidth
 import numpy as np
 
 if __name__ == '__main__':
-    ## step 1: 加载数据
+    ## step 1: Load data
     print "step 1: load data..."
 
     dataSet = []
@@ -17,14 +17,14 @@ if __name__ == '__main__':
         dataSet.append([float(lineArr[0]), float(lineArr[1])])
 
     numSamples = len(dataSet)
-    X = np.array(dataSet) #列表类型转换成array数组类型
+    X = np.array(dataSet) # The list type is converted to an array array type
 
     bandwidth = estimate_bandwidth(X, quantile=0.2, n_samples=500)
     clf = MeanShift(bandwidth=bandwidth, bin_seeding=True,cluster_all=True).fit(X)
 
     centroids = clf.labels_
-    print centroids,type(centroids) #显示每一个点的聚类归属
-    # 计算其自动生成的k，并将聚类数量小于3的排除
+    print centroids,type(centroids) # Show the clustering of each point
+    # Calculate its automatically generated k, and the number of clusters less than 3 exclusion
     arr_flag = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     for i in clf.labels_:
         arr_flag[i]+=1
@@ -35,15 +35,15 @@ if __name__ == '__main__':
     print k
 
     mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
-    #画出所有样例点 属于同一分类的绘制同样的颜色
+    # Draw all the sample points that belong to the same class to draw the same color
     for i in xrange(numSamples):
         plt.plot(dataSet[i][0], dataSet[i][1], mark[clf.labels_[i]]) #mark[markIndex])
     mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']
-    # 画出质点，用特殊图型
+    # Draw a particle, with a special pattern
     centroids =  clf.cluster_centers_
     for i in range(k):
         plt.plot(centroids[i][0], centroids[i][1], mark[i], markersize = 12)
-    print centroids #显示中心点坐标
+    print centroids # Show center point coordinates
     plt.show()
 
 
